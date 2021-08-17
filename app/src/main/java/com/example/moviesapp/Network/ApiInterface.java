0dadcda157e95798079c4834d7641f9d42ddc5d4
manan0786 <m.manansaeed8@gmail.com>
2021-08-17 06:23:30 +0500

    first commit

---
+++ app/src/main/java/com/example/moviesapp/Network/ApiInterface.java
@@ -0,0 +1,4 @@
+package com.example.moviesapp.Network;
+
+public interface ApiInterface {
+}
---
+++ app/src/main/java/com/example/moviesapp/Network/ApiUri.java
@@ -0,0 +1,6 @@
+package com.example.moviesapp.Network;
+
+public class ApiUri {
+    public static final String BASE_URL = "https://api.themoviedb.org";
+    public static final String API_KEY = "1ec7736933b2485607e8f18a4d33124e";
+}
